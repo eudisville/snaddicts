58dad9dbfed8654cadaa2982983e7dc35a5b67b7
Eudes <mondesirkoffi30@gmail.com>
2024-09-05 07:26:31 +0000

    co

--- src/components/Topbar/Topbar.js
+++ src/components/Topbar/Topbar.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './Topbar.css'
 
 function Topbar() {
   return (
     <div className='bar'>
       <div className="left">
-        <h5>Bienvenue sur SNA | La boutique Officielle des Sneakers</h5>
+        <h5>Bienvenue sur SNA</h5>
         </div>
       <div className="right">
-        <h5>Abidjan, Côte d'Ivoire</h5>
+        <Link to="https://www.instagram.com/invites/contact/?igsh=yqfssme39lh&utm_content=uazcgys" target='_blank'><h5>Nos pages</h5></Link>
       </div>
     </div>
   )
